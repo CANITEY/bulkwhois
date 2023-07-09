2994cb5d67148e0eb2e2426bb90e69a51cfe561e
mohamedmed01345 <mohamedmed01345@gmail.com>
2023-07-09 14:52:32 +0300

    finished the script

--- go.mod
+++ go.mod
@@ -3,9 +3,15 @@ module github.com/mohamedmed01345/bulkwhois
 go 1.20
 
 require (
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/likexian/whois v1.15.0
+	github.com/likexian/whois-parser v1.24.8
+	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
+)
+
+require (
 	github.com/likexian/gokit v0.25.13 // indirect
-	github.com/likexian/whois v1.15.0 // indirect
-	github.com/likexian/whois-parser v1.24.8 // indirect
 	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,32 +1,105 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-    "flag"
+	"os"
+	"strings"
+	flag "github.com/jessevdk/go-flags"
 	"github.com/likexian/whois"
 	"github.com/likexian/whois-parser"
 )
 
-var name int
-func init() {
-    flag.IntVar(&name, "name", 4, "specify name")
-    flag.IntVar(&name, "n", 4, "specify name")
+// these are the parameters
+var opts struct {
+    File string `short:"f" long:"file" description:"specify file to \"whois\" it" value-name:"FILE"`
+    Out string  `short:"o" long:"output" description:"specify file to save the output" value-name:"FILE"`
+    All bool `short:"a" long:"all" description:"when this flag is used the script return every query with Organization name associated with it"`
+    Org_name string `short:"r" long:"org-name" description:"specify Organization name to return domains that match it" value-name:"\"ORG NAME\""`
+}
+
+//TODO: functions to return data
+func all(domain string) string {
+    domain = strings.TrimSpace(domain)
+    query, err := whois.Whois(domain)
+    if err != nil {
+        print(err)
+        return ""
+    }
+    parsed, err := whoisparser.Parse(query)
+    return fmt.Sprintf("%v\t\t%v",domain, parsed.Registrant.Organization) 
+}
+
+func returnOrg(domain string, orgName string) string {
+    domain = strings.TrimSpace(domain)
+    query, err := whois.Whois(domain)
+    if err != nil {
+        print(err)
+        return ""
+    }
+    parsed, err := whoisparser.Parse(query)
+    if parsed.Registrant.Organization == orgName {
+        return fmt.Sprintf("[] %v\n", domain)
+    }else {
+        return ""
+    }
 }
 
 func main() {
-    fmt.Print("enter domain >> ")
-    var domain string
-    fmt.Scan(&domain)
-    result, err := whois.Whois(domain)
-    if err != nil {
-        fmt.Println(err)
+    fmt.Println(`
+██████╗ ██╗   ██╗██╗     ██╗  ██╗██╗    ██╗██╗  ██╗ ██████╗ ██╗███████╗
+██╔══██╗██║   ██║██║     ██║ ██╔╝██║    ██║██║  ██║██╔═══██╗██║██╔════╝
+██████╔╝██║   ██║██║     █████╔╝ ██║ █╗ ██║███████║██║   ██║██║███████╗
+██╔══██╗██║   ██║██║     ██╔═██╗ ██║███╗██║██╔══██║██║   ██║██║╚════██║
+██████╔╝╚██████╔╝███████╗██║  ██╗╚███╔███╔╝██║  ██║╚██████╔╝██║███████║
+╚═════╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚══╝╚══╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝╚══════╝
+BY: CANITEY `)
+    // this part parses the arguments
+    _, err := flag.Parse(&opts)
+    if err != nil { return }
+    if !opts.All && opts.Org_name == "" {
+        fmt.Println("use -h/--h flag to print the help message")
         return
     }
-    rslt, err := whoisparser.Parse(result)
-    if err != nil {
-        fmt.Println(err)
+    // this part for file/stdin reading
+    var f *os.File
+    if opts.File != "" {
+        f, err = os.Open(opts.File)
+        if err != nil { return }
+        defer f.Close()
+    }else {
+        f = os.Stdin
+        defer f.Close()
+    }
+    if opts.All && opts.Org_name != "" {
+        fmt.Println("ERROR: Arguments collision (you can't use -a/--all and -r/--org together)")
         return
     }
-    fmt.Println(rslt.Registrant.Organization)
-    fmt.Println(name)
+
+    var outFile *os.File
+    if opts.Out != "" {
+        outFile, err = os.Create(opts.Out)
+        if err != nil {
+            return
+        }
+        defer outFile.Close()
+    }
+
+    scanner := bufio.NewScanner(f)
+    for scanner.Scan() {
+        if opts.All {
+            query := all(scanner.Text())
+            fmt.Println(query)
+            if opts.Out != "" {
+                outFile.WriteString(query + "\n")
+            }
+        } else if opts.Org_name != "" {
+            query := returnOrg(scanner.Text(), opts.Org_name)
+            fmt.Print(query)
+            if opts.Out != "" {
+                outFile.WriteString(query + "\n")
+            }
+        }
+    }
+
 }
